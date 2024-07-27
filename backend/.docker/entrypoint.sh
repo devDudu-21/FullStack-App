e12e601c01c274ce8178b88a2e8020f6911391fe
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-27 19:33:38 -0300

    fix error on entrypoint.sh adding sleep loop to waiting db

--- backend/.docker/entrypoint.sh
+++ backend/.docker/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sleep 5
+echo "Waiting for the database to start..."
+sleep 10
+echo "Database should be ready, running migrations..."
+
 npm install
 npx prisma migrate dev
 npm run build
